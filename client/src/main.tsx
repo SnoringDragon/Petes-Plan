7149fbcbd0b4a738ef8b45639ba28daf1f904eeb
patricia-casaca <70249618+patricia-casaca@users.noreply.github.com>
2023-02-16 23:37:41 +0000

    Adding login page try 1

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-    createBrowserRouter,
-    RouterProvider,
+  createBrowserRouter,
+  RouterProvider,
 } from 'react-router-dom';
 import App from './App';
 import './index.scss';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { Login } from './pages/login/login';
 
 const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [{
+      index: true,
+      element: <Dashboard />
+    },
     {
-        path: '/',
-        element: <App />,
-        children: [{
-            index: true,
-            element: <Dashboard />
-        }]
+      path: '/new-page',
+      element: <Login />
     }
+    ]
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
