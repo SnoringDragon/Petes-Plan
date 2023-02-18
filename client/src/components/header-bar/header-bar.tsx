f172bdf969ea45bf31dfbc0796d7f74775ddc583
sk-1982 <13491412+sk-1982@users.noreply.github.com>
2023-02-18 17:11:24 -0500

    Add login and register links to header

--- client/src/components/header-bar/header-bar.tsx
+++ client/src/components/header-bar/header-bar.tsx
@@ -1,5 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export function HeaderBar() {
     return (<div className="h-16 p-4 flex items-center border-b border-slate-600 bg-neutral-700">
         title
+        <div className="ml-auto flex">
+            <Link to="/login" className="mr-4">Login</Link>
+            <Link to="/register">Register</Link>
+        </div>
     </div>);
 }
