60b999f6369a676f8f5122b6215facbe581ebeb6
sk-1982 <13491412+sk-1982@users.noreply.github.com>
2023-02-18 16:59:03 -0500

    Add layout component for optional sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import './App.scss'
-import { Sidebar } from './components/sidebar/sidebar';
 import { HeaderBar } from './components/header-bar/header-bar';
 
 function App() {
   return (
-    <div className="flex">
-      <Sidebar />
-      <div className="flex flex-col w-full  ">
-        <HeaderBar />
-        <div className="h-100 p-4 overflow-y-auto">
-          <Outlet />
-        </div>
-      </div>
+    <div className="flex flex-col h-full">
+      <HeaderBar />
+        <Outlet />
     </div>
   )
 }
