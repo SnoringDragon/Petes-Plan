751410b601bc4aaede551e51a7a9f76b30631c15
patricia-casaca <70249618+patricia-casaca@users.noreply.github.com>
2023-02-19 22:16:41 +0000

    Adding stay signed in and the clcik here if you forgot your password.

--- client/src/pages/login/login.tsx
+++ client/src/pages/login/login.tsx
@@ -1,8 +1,3 @@
-/*
-export function Login() {
-    return (<div>login in</div>);
-}
-*/
 import React, { useReducer, useEffect } from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
@@ -11,7 +6,53 @@ import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import CardHeader from '@material-ui/core/CardHeader';
 import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
 
+export function Login() {
+    return (<div className="w-full h-full flex items-center justify-center">
+        <Card className="-mt-16">
+            <CardHeader title="Login" className="text-center bg-zinc-800 text-white" />
+            <CardContent>
+                <div className="p-4">
+                    <TextField
+                        fullWidth
+                        id="username"
+                        type="email"
+                        label="Username"
+                        placeholder="Username"
+                        margin="normal"
+                    />
+                    <TextField
+                        fullWidth
+                        id="password"
+                        type="password"
+                        label="Password"
+                        placeholder="Password"
+                        margin="normal"
+                    />
+                    <p> </p>
+                    <a href='/sign_up'><u><br />Do not have an account? Sign up here.</u></a >
+                    <a href='/dashboard'><u><br />Forgot your password? Click here.<br /></u></a >
+                    <Checkbox
+                    //handleChange={handleChangeA}
+                    //isChecked={isCheckedA}
+                    />
+                    <text>Stay signed in.</text>
+                </div>
+            </CardContent>
+            <CardActions>
+                <Button
+                    variant="contained"
+                    size="large"
+                    color="secondary"
+                    className="w-full">
+                    Login
+                </Button>
+            </CardActions>
+        </Card>
+    </div>)
+}
+/*
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         container: {
@@ -72,3 +113,4 @@ export function Login() {
             </CardActions>
         </Card>);
 }
+*/
