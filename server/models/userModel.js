62881b4f0d06182f1f2bdc80a1603e190033dc63
sk-1982 <13491412+sk-1982@users.noreply.github.com>
2023-02-18 19:22:32 -0500

    WIP password hashing

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -5,10 +5,22 @@ const mongoose = require('mongoose');
 /* Create a user schema */
 const userSchema = new mongoose.Schema({
     email: String,
-    passwordSalt: String,
-    passwordHash: String,
+    password: String,
     verified: Boolean,
     verificationToken: String,
 });
+
+// https://stackoverflow.com/questions/14588032/mongoose-password-hashing
+userSchema.pre('save', async function (next) {
+    // don't need to re-hash password if password not modified
+    if (!this.isModified('password')) return next();
+
+
+});
+
+userSchema.methods.validatePassword = async function (data) {
+    // TODO: check password to see if it matches hash
+    return true;
+}
 
 module.exports = mongoose.model('User', userSchema);
