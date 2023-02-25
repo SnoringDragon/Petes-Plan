455065dbeee706037f68f0eb7225175581270485
SnoringDragon <89707895+SnoringDragon@users.noreply.github.com>
2023-02-25 00:17:07 -0500

    Restructured HTTP Responses Added Reset Password Emailing Added Reset Password Submission

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -24,6 +24,7 @@ exports.signup = async (req, res) => {
     }
     
     /* Validate email address */
+    //TODO: Check if email matches a known university domain
     const email = req.body.email.toLowerCase();
     if (!email.match(emailRegex)) {
         return res.status(400).json({
@@ -51,23 +52,23 @@ exports.signup = async (req, res) => {
         });
 
         /* Save the user to the database */
-        user.save().then(() => {
-            res.status(201).json({
+        user.save().then(async (req, res) => {
+            /* Send a verification email */
+            mailer.sendEmail(email, 'Email Verification', 'verifyEmail', {
+                username: email.substring(0, email.indexOf('@')),
+                email: email,
+                token: await token
+            });
+
+            return res.status(201).json({
                 message: 'User added successfully!'
             });
         }).catch((error) => {
             console.log(error);
-            res.status(500).json({
+            return res.status(500).json({
                 error: 'Could not create user'
             });
         });
-    });
-
-    /* Send a verification email */
-    mailer.sendEmail(email, 'Email Verification', 'verifyEmail', {
-        username: email.substring(0, email.indexOf('@')),
-        email: email,
-        token: await token
     });
 };
 
@@ -75,36 +76,39 @@ exports.signup = async (req, res) => {
 exports.verifyEmail = async (req, res) => {
     /* Checks if the email and token are provided */
     if (!req.query.email || !req.query.token) {
-        res.status(400).json({
+        return res.status(400).json({
             error: 'Missing email or token'
         });
         return;
     }
-    
+
     User.find({ email: req.query.email.toLowerCase() }, (err, docs) => {
         if (err) {
-            //TODO Set up error handling
             console.log(err);
-            res.status(500).json({
+            return res.status(500).json({
                 error: 'Internal Server Error'
             });
-            return;
         }
         const user = docs[0];
 
         /* Check if the user exists and token matches */
-        if (user && (user.verificationToken === req.query.token)) {
+        if (user && (user.verified === false) && (user.verificationToken === req.query.token)) {
             /* Update the user's details */
             user.verified = true;
             user.verificationToken = '';
-            user.save();
+            user.save().catch((err) => {
+                console.log(err);
+                return res.status(500).json({
+                    error: 'Internal Server Error'
+                });
+            });
 
-            res.status(200).json({
+            return res.status(200).json({
                 message: 'Email verified successfully'
             });
         } else {
-            res.status(400).json({
-                error: 'Invalid email or token'
+            return res.status(400).json({
+                error: 'Invalid email token pair'
             });
         }
     });
@@ -147,11 +151,91 @@ exports.login = async (req, res) => {
     });
 };
 
-/* Verify a user's token */
-exports.verifyToken = async (req, res) => {
-    // authenticate middleware added in userController
-    // therefore already authenticated if we are at this point
-    return res.json({ error: false });
+/* Send user a password reset email */
+exports.resetRequest = async (req, res) => {
+    /* Check if the user has provided an email */
+    if (!req.body.email) {
+        return res.status(400).json({
+            error: 'No email provided'
+        });
+        return;
+    }
+    
+    /* Validate email address */
+    const email = req.body.email.toLowerCase();
+    if (!email.match(emailRegex)) {
+        return res.status(400).json({
+            error: 'Invalid email address'
+        });
+        return;
+    }
+
+    /* Check if the user already exists */
+    const user = await User.findOne({ email: email });
+    if (user && user.verified) {
+        /* Generate a new token */
+        const token = generateToken(email, '24 hours');
+
+        /* Update the user's details */
+        user.verificationToken = await token;
+        user.save().catch((error) => {
+            console.log(error);
+            return res.status(500).json({
+                error: 'Internal Server Error'
+            });
+        });
+
+        /* Send a reset email */
+        mailer.sendEmail(email, 'Password Reset', 'resetPassword', {
+            username: email.substring(0, email.indexOf('@')),
+            email: email,
+            token: await token
+        });
+    }
+
+    return res.status(201).json({
+        message: 'Password reset email sent'
+    });
+};
+
+/* Reset a user's password */
+exports.resetPassword = async (req, res) => {
+    /* Check if the user has provided an email, token, and new password */
+    if (!req.body.email || !req.body.token || !req.body.password) {
+        return res.status(400).json({
+            error: 'Missing email, token, or password'
+        });
+    }
+
+    /* Validate email address */
+    const email = req.body.email.toLowerCase();
+    if (!email.match(emailRegex)) {
+        return res.status(400).json({
+            error: 'Invalid email address'
+        });
+    }
+
+    /* Check if the user exists and token matches */
+    const user = await User.findOne({ email: email });
+    if (user && (user.verified === true) && (user.verificationToken === req.body.token)) {
+        /* Update the user's details */
+        user.password = await bcrypt.hash(req.body.password, 10);
+        user.verificationToken = '';
+        user.save().catch((err) => {
+            console.log(err);
+            return res.status(500).json({
+                error: 'Internal Server Error'
+            });
+        });
+
+        return res.status(201).json({
+            message: 'Password reset successfully'
+        });
+    } else {
+        return res.status(400).json({
+            error: 'Invalid email token pair'
+        });
+    }
 };
 
 /* Logout a user */
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -6,13 +6,14 @@ module.exports = app => {
     const userController = require('../controllers/userController');
 
     /* Add HTTP requests for managing user data */
-    router.post('/signup', userController.signup);              /* Create a new user */
-    router.get('/verifyemail', userController.verifyEmail);                 /* Verify a user's email */
-    router.post('/login', userController.login);                /* Login a user */
-    router.get('/verifytoken', authenticate, userController.verifyToken);     /* Verify a user's token */
-    router.post('/logout', authenticate, userController.logout);              /* Logout a user */
-    router.delete('/delete', authenticate, userController.delete);            /* Delete a user */
-    router.put('/update', authenticate, userController.update);               /* Update a user's details */
+    router.post('/signup', userController.signup);                  /* Create a new user */
+    router.get('/verifyemail', userController.verifyEmail);         /* Verify a user's email */
+    router.post('/login', userController.login);                    /* Login a user */
+    router.post('/resetrequest', userController.resetRequest);      /* Send email to reset password */
+    router.post('/resetpassword', userController.resetPassword);    /* Reset a user's password */
+    router.post('/logout', authenticate, userController.logout);    /* Logout a user */
+    router.delete('/delete', authenticate, userController.delete);  /* Delete a user */
+    router.put('/update', authenticate, userController.update);     /* Update a user's details */
 
     /* Add above reuests as a subdirectory of /user */
     app.use('/api/user', router);
