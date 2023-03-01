35ae4b197658a398881cfcdcd29ecf1f118569bd
SnoringDragon <89707895+SnoringDragon@users.noreply.github.com>
2023-03-01 12:00:27 -0500

    Fixed degreePlanRoute comments

--- server/routes/degreePlanRoute.js
+++ server/routes/degreePlanRoute.js
@@ -10,12 +10,11 @@ module.exports = app => {
     router.post('/create', authenticate, controller.addDegreePlan);         /* Creates a new degree plan */
     router.delete('/delete', authenticate, controller.deleteDegreePlan);    /* Deletes existing degree plan */
 
+    /* Add HTTP requests for managing courses in a degree plan */
     router.post('/*/add', authenticate, controller.addCourse);              /* Adds a course to a degree plan */
     router.post('/*/modify', authenticate, controller.modifyCourse);        /* Updates a course in a degree plan (e.g. grade, semester, etc.) */
-    router.delete('/*/remove', authenticate, controller.removeCourse);         /* Removes a course from a degree plan */
+    router.delete('/*/remove', authenticate, controller.removeCourse);      /* Removes a course from a degree plan */
     router.get('/*', authenticate, controller.getDegreePlan);               /* Returns degree plan data */
-
-    
 
     /* Add above reuests as a subdirectory of /degreePlan */
     app.use('/api/degreePlan', router);
