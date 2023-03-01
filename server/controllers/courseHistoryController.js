6ada6253c216e24d3f7ff2e6f9351feb2137afe2
SnoringDragon <89707895+SnoringDragon@users.noreply.github.com>
2023-02-28 22:56:03 -0500

    Added course history modification

--- server/controllers/courseHistoryController.js
+++ server/controllers/courseHistoryController.js
@@ -1,4 +1,5 @@
-const userCourse = require('../models/userCourseModel');
+const UserCourse = require('../models/userCourseModel');
+const User = require('../models/userModel');
 
 /* Get all courses the user has previously taken */
 exports.getCourses = async (req, res) => {
@@ -49,7 +50,7 @@ exports.addCourse = async (req, res) => {
                 message: `Grade must be a number`,
                 course: course
             });
-        }
+        } else course.grade = null;
 
         /* Validate section is a number */
         if (course.section && isNaN(course.section)) {
@@ -57,7 +58,7 @@ exports.addCourse = async (req, res) => {
                 message: `Section must be a number`,
                 course: course
             });
-        }
+        } else course.section = null;
 
         /* Validate semester is valid */
         if (isNaN(course.year)) {
@@ -78,7 +79,7 @@ exports.addCourse = async (req, res) => {
         }
 
         /* Add the course to the user's current degree plan */
-        user.completedCourses.push(new userCourse({
+        user.completedCourses.push(new UserCourse({
             courseID: course.courseID,
             semester: course.semester,
             year: course.year,
@@ -110,7 +111,82 @@ exports.addCourse = async (req, res) => {
 
 /* Modify a course in the user's current degree plan */
 exports.modifyCourse = async (req, res) => {
+    const user = req.user;
+    const courses = req.body.courses;
+    var updated = false;
 
+    /* Check if courses are provided */
+    if (!courses || !(courses instanceof Array) || courses.length === 0) {
+        return res.status(400).json({
+            message: 'Missing array of courses'
+        });
+    }
+
+    /* Update each provided course */
+    for (let i = 0; i < courses.length; i++) {
+        const course = courses[i];
+
+        /* Validate required fields present */
+        if (!course || !course._id) {
+            return res.status(400).json({
+                message: `Missing course _id`,
+                course: course
+            });
+        }
+
+        /* Get corresponding course in user's current degree plan */
+        const orgCourse = await user.completedCourses.id(course._id);
+
+        /* Validate course exists in current degree plan */
+        if (!orgCourse) {
+            return res.status(400).json({
+                message: `Invalid course _id`,
+                course: course
+            });
+        }
+
+        /* Validate grade is a new number */
+        if ((typeof course.grade !== 'undefined') && (course.grade !== orgCourse.grade)) {
+            if (isNaN(course.grade)) {
+                return res.status(400).json({
+                    message: `Grade must be a number`,
+                    course: course
+                });
+            }
+            orgCourse.grade = course.grade;
+            updated = true;
+        }
+
+        /* Validate section is a new number */
+        if ((typeof course.section !== 'undefined') && (course.section !== orgCourse.section)) {
+            if (isNaN(course.section)) {
+                return res.status(400).json({
+                    message: `Section must be a number`,
+                    course: course
+                });
+            }
+            orgCourse.section = course.section;
+            updated = true;
+        }
+    }
+
+    /* Save the user to the database */
+    if (updated) {
+        user.save().then(() => {
+            return res.status(200).json({
+                message: 'Course(s) updated in current degree plan'
+            });
+        }).catch((err) => {
+            console.log(err.message);
+            return res.status(500).json({
+                message: 'Internal Server Error'
+            });
+        });
+    } else {
+        return res.status(200).json({
+            message: 'No changes made to course(s)'
+        });
+    }
 };
 
 /* Delete a course from the user's current degree plan */
--- server/routes/courseHistoryRoute.js
+++ server/routes/courseHistoryRoute.js
@@ -6,11 +6,11 @@ module.exports = app => {
     const controller = require('../controllers/courseHistoryController');
 
     /* Add HTTP requests for managing user data */
-    router.get('/', authenticate, controller.getCourses);                   /* Returns all courses the user has previously taken */
-    router.get('/query', authenticate, controller.queryCourses);            /* Returns all courses the user has previously taken that match the query */
-    router.post('/addCourse', authenticate, controller.addCourse);          /* Adds new course to historical plan */
-    router.post('/modifyCourse', authenticate, controller.modifyCourse);    /* Modifies existing course in historical plan */
-    router.delete('/deleteCourse', authenticate, controller.deleteCourse);  /* Deletes existing course in historical plan */
+    router.get('/', authenticate, controller.getCourses);               /* Returns all courses the user has previously taken */
+    router.get('/query', authenticate, controller.queryCourses);        /* Returns all courses the user has previously taken that match the query */
+    router.post('/add', authenticate, controller.addCourse);            /* Adds new course to historical plan */
+    router.post('/modify', authenticate, controller.modifyCourse);      /* Modifies existing course in historical plan */
+    router.delete('/delete', authenticate, controller.deleteCourse);    /* Deletes existing course in historical plan */
 
     /* Add above reuests as a subdirectory of /user */
     app.use('/api/courseHistory', router);
