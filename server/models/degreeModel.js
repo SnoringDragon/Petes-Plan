d051d4689578e34821776fb92afcedddda3d99fe
anushka48 <sharm495@purdue.edu>
2023-03-02 17:41:34 -0500

    Added all degree data for Purdue CS for testing

--- server/models/degreeModel.js
+++ server/models/degreeModel.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const degreeSchema = new mongoose.Schema({
     name: String,
-    type: ['major', 'minor', 'concentration', 'certificate'],
+    type: {
+        type: String,
+        enum: ['major', 'minor', 'concentration', 'certificate']
+    },
     requirements: [String], //array of courseIDs
     concentrations: [degreeSchema]
 });
--- server/scripts/populate-test.js
+++ server/scripts/populate-test.js
@@ -1,17 +1,75 @@
 const mongoose = require('mongoose');
-const courseModel = require('../models/courseModel');
+const degreeModel = require('../models/degreeModel');
 
-const names = ["Problem Solving And Object-Oriented Programming", "Foundations Of Computer Science", 
-"Tools", "Programming In C", "Introduction To Data Science", 
-"Computer Architecture", "Data Structures And Algorithms", 
-"Systems Programming", "Software Engineering I", "Fundamentals Of Computer Graphics", 
-"Information Systems", "Cloud Computing", "Compilers: Principles And Practice", "Operating Systems", 
-"Introduction To Cryptography", "Data Mining And Machine Learning", "Python Programming", 
-"Introduction To The Analysis Of Algorithms", "Competitive Programming II", "Great Issues In Computer Sci"]
+//All possible degrees in Purdue CS Department
+const names = ["Computer Science", "Data Science", 
+"Artificial Intelligence", "Computational Science and Engineering Track",
+"Computer Graphics and Visualization Track", "Database and Information Systems Track", 
+"(Algorithmic) Foundations Track", "Programming Language Track", 
+"Security Track", "Software Engineering Track", "Systems Software Track", "Machine Intelligence Track"]
 
-let subject = 'CS'
-const courseIDs = ['18000', '18200', '19300', '24000', '24200', '25000', '25100', '25200', "30700", "33400",
-"34800", "35100", "35200", "35400", "35500", "37300", "38003", "38100", "39000-CP2", "CS 39000-GIS"]
+let types = ['major','concentration'];
+let requirements = [['MA 261', 'MA 35100', 'CS 18000', 'CS 18200', 'CS 24000', 'CS 25000', 'CS 25100', 'CS 25200'], 
+['CS 18000', 'CS 18200', 'CS 19100', 'CS 19300', 'CS 24200', 'CS 25100', 'CS 38003','MA 26100', 'MA 35100', 'STAT 35500', 'STAT 41600', 'STAT 41700', 'CS 490', 'PHIL 208'], 
+['CS 17600', 'PSY 12000', 'CS 18000', 'CS 18200', 'CS 24200', 'MA 26100', 'PHIL 20700', 'CS 25100', 'MA 26500', 'STAT 41600', 'PHIL 22100', 'CS 37300', 'CS 38100', 'CS 47100'],
+['MA 26600', 'CS 31400', 'CS 30700', 'CS 33400', 'CS 35200', 'CS 35400', 'CS 38100'],
+['CS 33400', 'CS 31400', 'CS 31400','CS 35200','CS 35400','CS 38100','CS 42200','CS 43400','CS 44800','CS 47100','CS 49000'],
+['CS 34800', 'CS 38100', 'CS 44800', 'CS 37300','CS 35200','CS 35500','CS 37300'],
+['CS 35200', 'CS 37300', 'CS 38100', 'CS 31400','CS 33400','CS 35300'],
+['CS 35200', 'CS 35400', 'CS 45600', 'CS 30700', 'CS 35300', 'CS 38100', 'CS 42200', 'CS 48300'],
+['CS 35400','CS 35500','CS 42600','CS 30700', 'CS 34800', 'CS 35200'],
+['CS 30700', 'CS 35200', 'CS 40800', 'CS 40700', 'CS 34800', 'CS 35300', 'CS 35400'],
+['CS 35200', 'CS 35400', 'CS 42200', 'CS 30700', 'CS 33400', 'CS 35300'],
+['CS 37300','CS 38100', 'CS 47100', 'STAT 41600', 'CS 34800', 'CS 35200']];
+// let concentrations = names.slice(3);
 
-const newCourse = new courseModel({ size: 'small' });
+const cseTrack = degreeModel.create({ name: names[3], type: types[1], requirements: requirements[3]}, function (err) {
+    if (err) return handleError(err);
+    // saved!
+    });
 
+const cgvtTrack = degreeModel.create({ name: names[4], type: types[1], requirements: requirements[4]}, function (err) {
+    if (err) return handleError(err);
+    // saved!
+    });
+
+const disTrack = degreeModel.create({ name: names[5], type: types[1], requirements: requirements[5]}, function (err) {
+    if (err) return handleError(err);
+    // saved!
+    });
+const afTrack = degreeModel.create({ name: names[6], type: types[1], requirements: requirements[6]}, function (err) {
+    if (err) return handleError(err);
+    // saved!
+    });
+const plTrack = degreeModel.create({ name: names[7], type: types[1], requirements: requirements[7]}, function (err) {
+    if (err) return handleError(err);
+        // saved!
+    });
+const sTrack = degreeModel.create({ name: names[8], type: types[1], requirements: requirements[8]}, function (err) {
+    if (err) return handleError(err);
+        // saved!
+    });
+const seTrack = degreeModel.create({ name: names[9], type: types[1], requirements: requirements[9]}, function (err) {
+    if (err) return handleError(err);
+            // saved!
+    });
+const ssTrack = degreeModel.create({ name: names[10], type: types[1], requirements: requirements[10]}, function (err) {
+    if (err) return handleError(err);
+            // saved!
+    });
+const miTrack = degreeModel.create({ name: names[11], type: types[1], requirements: requirements[11]}, function (err) {
+        if (err) return handleError(err);
+                // saved!
+        });
+const cs = degreeModel.create({ name: names[0], type: types[0], requirements: requirements[0], concentration: [cseTrack, cgvtTrack, disTrack, afTrack, plTrack, sTrack, seTrack, ssTrack, miTrack]}, function (err) {
+    if (err) return handleError(err);
+            // saved!
+    });
+const ds = degreeModel.create({ name: names[1], type: types[0], requirements: requirements[1]}, function (err) {
+    if (err) return handleError(err);
+            // saved!
+    });
+const ai = degreeModel.create({ name: names[2], type: types[0], requirements: requirements[2]}, function (err) {
+    if (err) return handleError(err);
+            // saved!
+    });
