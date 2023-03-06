8530b2613bfcdef4742864d1811eacfa7ac494b1
anushka48 <sharm495@purdue.edu>
2023-03-05 23:15:20 -0500

    started GPACalculator

---
+++ server/utils/GPACalc.js
@@ -0,0 +1,14 @@
+const courseModel = require('../models/courseModel');
+const usercourseModel = require('../models/userCourseModel');
+
+//create dummy data
+
+//input - userCourse list, gather quality points for each
+const input = [];
+
+//map each userCourse to its course, gather credit hours for each
+
+// dividing your quality points by your GPA hours
+function calculateCumulativeGPA(gpas) {
+    GPA hours 
+}
