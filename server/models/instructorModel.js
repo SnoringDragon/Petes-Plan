71239b822b8c127ca07d08d8c23a312e1673e0de
sk-1982 <13491412+sk-1982@users.noreply.github.com>
2023-03-24 21:43:20 -0400

    Add text index for instructors

--- server/models/instructorModel.js
+++ server/models/instructorModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const instructorSchema = new mongoose.Schema({
     firstname: String,
     lastname: String,
+    nickname: String,
     email: {
         type: String,
         unique: true,
@@ -16,6 +17,9 @@ const instructorSchema = new mongoose.Schema({
 });
 
 instructorSchema.index({ firstname: 1, lastname: 1 });
+instructorSchema.index({ firstname: 'text', lastname: 'text', nickname: 'text' }, {
+    default_language: 'none' // dont stem names
+});
 
 
 module.exports = mongoose.model('Instructor', instructorSchema);
