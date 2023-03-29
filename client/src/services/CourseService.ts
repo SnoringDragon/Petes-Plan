20aa145150ef488b0bc6463758063d77f3dc5831
sk-1982 <13491412+sk-1982@users.noreply.github.com>
2023-03-29 15:12:34 -0400

    add comment for sections

--- client/src/services/CourseService.ts
+++ client/src/services/CourseService.ts
@@ -6,6 +6,22 @@ class CourseService extends Api {
         return this.get(`/api/courses/?${new URLSearchParams(options)}`);
     }
 
+    /**
+     * Get sections
+     * @param options
+     * @return section outer array is group to choose one, next group is grouped by section type,
+     *  inner group is list of section types to choose one. Example: [
+     *      [
+     *          [lecture 1, lecture 2],
+     *          [recitation 1, recitation 2]
+     *      ],
+     *      [
+     *          [lecture 3, lecture 4],
+     *          [recitation 3, recitation 4]
+     *      ]
+     *  ]
+     *  A valid choice could be lecture 1 + recitation 1. another valid choice could be lecture 3 + recitation 4
+     */
     getCourseSections(options: { courseID: string, subject: string, semester: string }) {
         return this.get<Section[][][]>(`/api/courses/sections/?${new URLSearchParams(options)}`);
     }
