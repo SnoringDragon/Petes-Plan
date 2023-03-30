1d1e971bf962a50e8c0fcd18531d45cbaa422526
anushka48 <sharm495@purdue.edu>
2023-03-30 19:08:49 -0400

    Finished course BoilerGrades fetch

--- server/services/boiler-grades.js
+++ server/services/boiler-grades.js
@@ -1,11 +1,10 @@
-//https://www.boilergrades.com/api/grades?instructor=Pushkar,%20Yulia%20N.
-
 let apiResponse;
-let url;
 var XMLHttpRequest = require('xhr2');
 var xhr = new XMLHttpRequest();
 
+// ------------ INSTRUCTOR REQUESTS FROM BG -------------------
 //Main function is getBGInstructor(fName, mName, lName) //all fields are required but if no middle name then input null
+
 function getInstructorURL(fName, mInit = null, lName) {
     let instructorURL = "https://www.boilergrades.com/api/grades?instructor="+lName+",%20"+fName;
     if (mInit) {
@@ -14,8 +13,8 @@ function getInstructorURL(fName, mInit = null, lName) {
     return instructorURL;
 }
 
-function getInstructorData(fName, mInit = null, lName, callback) {
-    url = getInstructorURL(fName, mInit, lName);
+function fetchInstructorData(fName, mInit = null, lName, callback) {
+    let url = getInstructorURL(fName, mInit, lName);
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
       if (xhr.readyState === XMLHttpRequest.DONE) {
@@ -33,15 +32,46 @@ function getInstructorData(fName, mInit = null, lName, callback) {
   
 function returnData(data) {
     // Do something with the data
-    console.log(data[0]);
-    return(data[0])
+    console.log(data);
+    return(data)
 }
 
 function getBGInstructor(fName = "Sula", mName = null, lName = "Lee") {
-    getInstructorData(fName = "Sula", mName = null, lName = "Lee", callback = returnData);
+    fetchInstructorData(fName = "Sula", mName = null, lName = "Lee", callback = returnData);
 }
 
+// ------------ COURSE REQUESTS FROM BG -------------------
+//Main function is getBGCourse(courseID)
+
+function getCourseURL(courseID) {
+    let courseIDComponents = courseID.match(/[a-zA-Z]+|[0-9]+/g)
+    let courseURL = "https://www.boilergrades.com/api/grades?course="+courseIDComponents[0]+"%20"+courseIDComponents[1];
+    return courseURL;
+}
+
+function getCourseData(courseID, callback) {
+    let url = getCourseURL(courseID);
+    console.log(url)
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          var response = JSON.parse(xhr.responseText);
+          callback(response);
+        } else {
+          console.error('Error fetching data. Status:', xhr.status);
+        }
+      }
+    };
+    xhr.open('GET', url, true);
+    xhr.send();
+}
+
+function getBGCourse(courseID) {
+    getCourseData(courseID, callback = returnData);
+}
+
+// Instruc Ex: console.log("inside, ", getBGInstructor(fName = "Sula", mName = null, lName = "Lee"));
+// Course Ex: console.log(getBGCourse("BIOL 11000"))
 module.exports = getBGInstructor;
-
-// console.log("inside, ", getBGInstructor(fName = "Sula", mName = null, lName = "Lee"));
-
+module.exports = getBGCourse;
