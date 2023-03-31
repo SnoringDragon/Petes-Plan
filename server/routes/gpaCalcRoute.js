bc30a1e0cc316f37a095a328a65402944217e18b
anushka48 <sharm495@purdue.edu>
2023-03-30 21:01:42 -0400

    Added GPARoute

--- server/routes/gpaCalcRoute.js
+++ server/routes/gpaCalcRoute.js
@@ -1,13 +1,31 @@
 const { Router } = require('express');
-const Course = require('../models/courseModel');
-const Section = require('../models/sectionModel');
-const Semester = require('../models/semesterModel');
+const GPACalc = require('../utils/GPACalc');
 
 const mongoose = require('mongoose');
 
 module.exports = app => {
     const router = Router();
-    router.get('/', async (req, res) => {
-        if (typeof req.query.subject !== 'string' || typeof req.query.courseID !== 'string')
-        return res.status(400).json({ message: 'invalid input' });
-    }}
+    router.get('/cumulativeGPA', async (req, res) => {
+        const gpa = await GPACalc.cumulativeGPA(req, res);
+        return gpa;
+    });
+
+    router.get('/semesterGPA', async (req, res) => {
+        if (typeof req.query.semesterInput !== 'string' || typeof req.query.yearInput !== int)
+            return res.status(400).json({ message: 'invalid semester input' });
+
+        const gpa = await GPACalc.semesterGPA(req, res, req.query.semesterInput, req.query.yearInput);
+        return gpa;
+    });
+
+    router.get('/majorGPA', async (req, res) => {
+        if (typeof req.query.major !== 'string')
+            return res.status(400).json({ message: 'invalid major input' });
+
+        const gpa = await GPACalc.majorGPA(req, res, req.query.major);
+        return gpa;
+    });
+
+    app.use('/api/GPA', router);
+
+}
