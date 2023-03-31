f78fb7decf3fed70c9dad17191a8653348dcce85
anushka48 <sharm495@purdue.edu>
2023-03-30 20:36:43 -0400

    Added class Grade Route

---
+++ server/routes/gpaCalcRoute.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const Course = require('../models/courseModel');
+const Section = require('../models/sectionModel');
+const Semester = require('../models/semesterModel');
+
+const mongoose = require('mongoose');
+
+module.exports = app => {
+    const router = Router();
+    router.get('/', async (req, res) => {
+        if (typeof req.query.subject !== 'string' || typeof req.query.courseID !== 'string')
+        return res.status(400).json({ message: 'invalid input' });
+    }}
