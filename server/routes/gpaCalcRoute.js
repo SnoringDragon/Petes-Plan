715ed6d900083cbdd5f4f0dee50686ab36f790a6
SnoringDragon <89707895+SnoringDragon@users.noreply.github.com>
2023-03-30 20:58:49 -0400

    Fixed syntax error in gpaCalcRoute

--- server/routes/gpaCalcRoute.js
+++ server/routes/gpaCalcRoute.js
@@ -7,7 +7,11 @@ const mongoose = require('mongoose');
 
 module.exports = app => {
     const router = Router();
+    
     router.get('/', async (req, res) => {
         if (typeof req.query.subject !== 'string' || typeof req.query.courseID !== 'string')
         return res.status(400).json({ message: 'invalid input' });
-    }}
+    });
+
+    app.use('/api/gpaCalc', router);
+}
