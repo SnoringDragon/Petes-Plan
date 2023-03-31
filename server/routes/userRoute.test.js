629daa88207b7295ebd0bd94e2b4beecfaa1fbfd
sk-1982 <13491412+sk-1982@users.noreply.github.com>
2023-03-30 20:21:53 -0400

    add tests for admin user creation

--- server/routes/userRoute.test.js
+++ server/routes/userRoute.test.js
@@ -1,13 +1,47 @@
-const { getLatestEmail } = require('../mail.jest')
+const { getLatestEmail } = require('../mail.jest');
+const jwt = require('jsonwebtoken');
 
 describe('account creation', () => {
     it('should create an admin account', async () => {
-        const res = await request(getApp()).post('/api/user/signup', {
+        const User = require('../models/userModel');
+
+        await User.deleteMany({});
+
+        await request(getApp()).post('/api/user/signup').send({
             email: process.env.ADMIN_EMAIL,
             password: 'hunter2',
             name: 'Admin'
-        });
+        }).expect(201);
 
-        console.log(res);
+        await request(getApp()).post('/api/user/signup').send({
+            email: "notanadmin@purdue.edu",
+            password: 'hunter2',
+            name: 'Not An Admin'
+        }).expect(201);
+
+        await User.updateMany({
+            email: { $in: [process.env.ADMIN_EMAIL, 'notanadmin@purdue.edu'] }
+        }, {
+            verified: true
+        });
     });
+
+    it('admin account should be an admin', async () => {
+        const res = await request(getApp()).post('/api/user/login')
+            .send({ email: process.env.ADMIN_EMAIL, password: 'hunter2' })
+            .expect(201);
+
+        const payload = jwt.decode(res.body.token);
+        expect(payload.isAdmin).toBe(true);
+    });
+
+    it('non-admin account should not be an admin', async () => {
+        const res = await request(getApp()).post('/api/user/login')
+            .send({ email: "notanadmin@purdue.edu", password: 'hunter2' })
+            .expect(201);
+
+        const payload = jwt.decode(res.body.token);
+        expect(payload.isAdmin).toBeFalsy();
+    });
+
 });
