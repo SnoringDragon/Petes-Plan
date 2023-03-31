a6c76c0792252b89f8b8a56e0d9a01bd2c4791d4
sk-1982 <13491412+sk-1982@users.noreply.github.com>
2023-03-31 08:54:20 -0400

    add boilergrades to instructor

--- client/src/components/boilergrades/boilergrades.tsx
+++ client/src/components/boilergrades/boilergrades.tsx
@@ -1,14 +1,15 @@
 import { BOILERGRADES_GRADES } from '../../types/boilergrades';
 import { Button } from '@material-ui/core';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export function Boilergrades(props: { className?: string,
-    isCourse?: boolean,
+    isCourseLinks?: boolean,
     data: Map<string, Partial<{ [key in keyof typeof BOILERGRADES_GRADES]: number }>> }) {
 
     const [isHidden, setHidden] = useState(false);
 
-    return (<div className="flex flex-col">
+    return (<div className={`flex flex-col ${props.className ?? ''}`}>
         <div className="flex items-center mb-2">
             <span className="underline mr-4">Boilergrades:</span>
             {props.data.size > 0 && <Button color="inherit" size="small" variant="outlined" onClick={() => setHidden(!isHidden)}>
@@ -16,10 +17,17 @@ export function Boilergrades(props: { className?: string,
             </Button>}
             {props.data.size === 0 && <span>No boilergrades found</span>}
         </div>
-        <div className={`flex flex-col h-64 overflow-auto ${isHidden ? 'hidden' : ''}`}>
-            {[...props.data].sort(([a], [b]) => a.localeCompare(b)).map(([title, grades], i) =>
-                <div key={i} className="flex items-center border border-gray-500">
-                <span className="w-64 pl-2">{title}</span>
+        <div className={`flex flex-col max-h-64 overflow-auto ${isHidden ? 'hidden' : ''}`}>
+            {[...props.data].sort(([a], [b]) => a.localeCompare(b)).map(([title, grades], i) => {
+                let courseID = '';
+                let subject = ''
+                if (props.isCourseLinks)
+                    [subject, courseID] = title.split(' ');
+
+                return <div key={i} className="flex items-center border border-gray-500">
+                    {props.isCourseLinks ? <Link  className="w-64 pl-2"  to={`/course_description?courseID=${courseID}&subject=${subject}`}>
+                        {title}
+                    </Link> : <span className="w-64 pl-2">{title}</span>}
                     <div className="flex">
                         {Object.entries(grades).map(([grade, val]) =>
                             <div className="flex flex-col w-16 items-center border-x border-gray-500" key={grade}>
@@ -27,7 +35,7 @@ export function Boilergrades(props: { className?: string,
                             <span>{(val).toFixed(1)}%</span>
                         </div>)}
                     </div>
-            </div>)}
+            </div>})}
         </div>
     </div>)
 }
