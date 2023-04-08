c612c03186cf330f59e8fbabbb5ad0042b887128
sk-1982 <13491412+sk-1982@users.noreply.github.com>
2023-04-08 01:40:38 -0400

    basic degree parsing support

--- server/services/acalog.js
+++ server/services/acalog.js
@@ -68,14 +68,62 @@ class Acalog extends BaseService {
         const $ = cheerio.load(await res.text());
 
         let aboutText = '';
+        let requiredCredits = 0;
+        let supergroups = [];
+        let lastSupergroup = null;
+        let lastGroup = null;
+        let lastRequirements = null;
+        let isChooseOne = false;
+        let previousCourseOr = false;
+
+        const updateRequirementToGroup = () => {
+            if (lastRequirements) {
+                if (!lastGroup) lastGroup = { requirements: [] };
+                lastGroup.requirements.push(lastRequirements);
+                lastRequirements = null;
+            }
+        };
+        const updateGroupToSupergroup = () => {
+            if (lastGroup) {
+                if (!lastSupergroup) return console.log('warning: no supergroup found for group to belong to');
+                lastSupergroup.groups.push(lastGroup);
+                lastGroup = null;
+            }
+        };
+        const updateSupergroups = () => {
+            if (lastSupergroup) {
+                supergroups.push(lastSupergroup);
+                lastSupergroup = null;
+            }
+        };
+
+        const getContents = row => $(row).find('> td > div').text().trim()
+            .replace(/[^\S\r\n]+/g, ' ')
+            .replace(/(?:[^\S\r\n]*\r?\n[^\S\r\n]*)+/g, '\n')
+            .trim() || null;
+
+        const getCredits = text => {
+            let credits = 0;
+
+            text = text.replace(/\s*\((\d+)\s*credits?\)\s*/ig, (_, c) => {
+                credits = +c;
+                return '';
+            });
+
+            return [credits, text];
+        };
+
+        let end = false;
 
         $('#acalog-degree-planner-content > tbody > tr').map((i, row) => {
+            if (end) return;
+
             const id = row.attribs.id;
 
             if (['acalog-degree-planner-logo', 'acalog-degree-planner-info'].includes(id)) return;
 
             if (id === 'acalog-degree-planner-programs') {
-                const about = $(row).find('div > p');
+                const about = $(row).find('div > :is(p, div)');
 
                 aboutText = about.map((_, el) => {
                     const text = $(el).text().trim();
@@ -84,7 +132,7 @@ class Acalog extends BaseService {
                     if (text.includes('(CODO) Requirements')) return;
 
                     return text.replace(/\s+/g, ' ').trim();
-                }).toArray().join('\n');
+                }).toArray().join('\n').trim();
 
                 return;
             }
@@ -98,10 +146,103 @@ class Acalog extends BaseService {
                 headerText = $(header).text();
             }
 
-            console.log(headerType, headerText)
+            if (headerType === 'h2' && ['Requirements', 'Degree Requirements'].includes(headerText)) {
+                const text = $(row).find('div h3').text();
+                const credits = text.match(/(\d+) Credits Required/)?.[1];
+                if (credits) requiredCredits = +credits;
+                return;
+            }
+
+            if (headerType === 'h2') {
+                let credits;
+                [credits, headerText] = getCredits(headerText);
+
+                updateRequirementToGroup();
+                updateGroupToSupergroup();
+                updateSupergroups();
+
+                lastSupergroup = {
+                    text: headerText,
+                    credits,
+                    groups: [],
+                    description: getContents(row),
+                    isChooseOne: /choose\s*one/i.test(headerText)
+                };
+
+                isChooseOne = false;
+                previousCourseOr = false;
+
+                return;
+            }
+
+            if (headerType === 'h3') {
+                if (headerText === 'Prerequisite Information:') {
+                    end = true;
+                    return;
+                }
+
+                let credits;
+                [credits, headerText] = getCredits(headerText);
+
+                updateRequirementToGroup();
+                updateGroupToSupergroup();
+
+                lastGroup = {
+                    text: headerText,
+                    credits,
+                    requirements: [],
+                    description: getContents(row)
+                };
+
+                isChooseOne = /choose\s*one/i.test(headerText);
+                previousCourseOr = false;
+
+                return;
+            }
+
+            const cell = $(row).find('> td').first();
+            const cellClass = cell.attr('class') ?? '';
+            const text = cell.text().trim()
+                .replace(/[^\S\r\n]+/g, ' ')
+                .replace(/(?:[^\S\r\n]*\r?\n[^\S\r\n]*)+/g, '\n');
+
+            if (cellClass.includes('course')) {
+                const [, subject, courseID] = text.match(/([A-Z]+) ([A-Z\d]+)/) ?? [];
+
+                if ((isChooseOne || previousCourseOr) && lastRequirements?.type === 'course') {
+                    lastRequirements.courses.push({ subject, courseID });
+                } else {
+                    updateRequirementToGroup();
+                    lastRequirements = {
+                        type: 'course',
+                        courses: [{ subject, courseID }]
+                    };
+                }
+
+                previousCourseOr = /\sor$/i.test(text);
+
+                return;
+            }
+
+            if (cellClass.includes('ad-hoc')) {
+                if (/choose\s*one/i.test(text)) {
+                    isChooseOne = true;
+                } else if (!/^(?:requirements|required):?$/i.test(text) && text) {
+                    isChooseOne = false;
+                    updateRequirementToGroup();
+                    lastRequirements = {
+                        type: 'non_course',
+                        text
+                    };
+                }
+            }
         });
 
-        return { about: aboutText };
+        updateRequirementToGroup();
+        updateGroupToSupergroup();
+        updateSupergroups();
+
+        return { about: aboutText, requiredCredits, supergroups };
     }
 }
 
