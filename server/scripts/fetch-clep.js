3e6ce25035703424f3eb3b172ceafbcb03fc78dd
anushka48 <sharm495@purdue.edu>
2023-04-09 22:51:49 -0400

    Added storage for CLEP tests

---
+++ server/scripts/fetch-clep.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+function getClepData() {
+    document.getElementById('info').innerHTML = "";
+        var myTab = document.getElementById('empTable');
+
+        // LOOP THROUGH EACH ROW OF THE TABLE AFTER HEADER.
+        for (i = 1; i < myTab.rows.length; i++) {
+
+            // GET THE CELLS COLLECTION OF THE CURRENT ROW.
+            var objCells = myTab.rows.item(i).cells;
+
+            // LOOP THROUGH EACH CELL OF THE CURENT ROW TO READ CELL VALUES.
+            for (var j = 0; j < objCells.length; j++) {
+                info.innerHTML = info.innerHTML + ' ' + objCells.item(j).innerHTML;
+            }
+            info.innerHTML = info.innerHTML + '<br />';     // ADD A BREAK (TAG).
+        }
+}
