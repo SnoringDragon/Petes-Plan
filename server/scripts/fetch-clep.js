0461a182371bc0c25da2ac3686039e0cbb4bb85e
anushka48 <sharm495@purdue.edu>
2023-04-09 22:57:58 -0400

    Updated CLEP Script

--- server/scripts/fetch-clep.js
+++ server/scripts/fetch-clep.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 function getClepData() {
-    document.getElementById('info').innerHTML = "";
-        var myTab = document.getElementById('empTable');
+    document.getElementById('info').innerHTML = "https://www.admissions.purdue.edu/transfercredit/clep.php?_ga=2.156040763.1830545689.1680885941-405777200.1660331919";
+        var table = document.getElementById('empTable');
 
         // LOOP THROUGH EACH ROW OF THE TABLE AFTER HEADER.
-        for (i = 1; i < myTab.rows.length; i++) {
+        for (i = 1; i < table.rows.length; i++) {
 
             // GET THE CELLS COLLECTION OF THE CURRENT ROW.
-            var objCells = myTab.rows.item(i).cells;
+            var tests = table.rows.item(i).cells;
 
             // LOOP THROUGH EACH CELL OF THE CURENT ROW TO READ CELL VALUES.
-            for (var j = 0; j < objCells.length; j++) {
+            for (var j = 0; j < tests.length; j++) {
                 //save to database
             }
             info.innerHTML = info.innerHTML + '<br />';     // ADD A BREAK (TAG).
