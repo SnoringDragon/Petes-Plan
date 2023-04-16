daa229f9d391b65a5104c439816e1f0390d860a6
SnoringDragon <89707895+SnoringDragon@users.noreply.github.com>
2023-04-16 17:20:55 -0400

    Added dynamic section blocks on schedule

--- client/src/pages/semester-calendar/semester-calendar.tsx
+++ client/src/pages/semester-calendar/semester-calendar.tsx
@@ -9,7 +9,6 @@ import { UserCourse } from "../../types/user-course";
 import "./semester-calendar.css";
 
 export function SemesterCalendar() {
-    // Semester Selection
     const [selectedSem, setSem] = useState<string>((): string => {
         const date = new Date();
         if (date.getMonth() <= 4) return "Spring";
@@ -21,7 +20,7 @@ export function SemesterCalendar() {
     const [semesters, setSemesters] = useState<Semester[]>([]);
     const [semester, setSemester] = useState<Semester>();
     const [degreePlans, setDegreePlans] = useState<DegreePlan[]>([]);
-    const degreePlan = useRef<DegreePlan>();
+    const [degreePlan, setDegreePlan] = useState<number>(0);
     const [courses, setCourses] = useState<UserCourse[]>([]);
     
     // Get semesters and degree plans from database on page load
@@ -30,12 +29,6 @@ export function SemesterCalendar() {
         SemesterService.getSemesters().then(res => setSemesters(res));
     }, []);
 
-    // Get degree plan for selected degree plan
-    useEffect(() => {
-        if (degreePlans.length > 0) degreePlan.current = degreePlans[0];
-        else degreePlan.current = undefined;
-    }, [degreePlans]);
-
     // Update semester when semester or year is changed
     useEffect(() => {
         setSemester(semesters.find(s => s.semester === selectedSem && s.year === selectedYear));
@@ -43,20 +36,37 @@ export function SemesterCalendar() {
 
     // Get courses for selected degreeplan and semester
     useEffect(() => {
-        if (!degreePlan.current) return;
+        if (degreePlans.length === 0) return;
         var courses: UserCourse[] = [];
-        degreePlan.current.courses.forEach(course => {
+        degreePlans[degreePlan].courses.forEach(course => {
             if (course.semester) {
                 if (course.semester === selectedSem && course.year === selectedYear) courses.push(course);
             }
         });
         setCourses(courses);
-    }, [semester, degreePlan.current]);
+    }, [semester, degreePlan, degreePlans]);
     
     // Render semester selection with error if semester is not found
     function renderSemesterSelection(): (undefined | JSX.Element) {
         const semesterOptions = ["Spring", "Summer", "Fall", "Winter"].map(semester => (<MenuItem value={semester}>{semester}</MenuItem>));
         
+        // Render degree plan select
+        function degreePlanSelect(): (undefined | JSX.Element) {
+            return (
+                <FormControl>
+                    <InputLabel id="degree-plan-select">Degree Plan</InputLabel>
+                    <Select
+                        id="outlined-input"
+                        labelId="degree-plan-select"
+                        value={degreePlan}
+                        onChange={e => setDegreePlan(e.target.value as number)}
+                    >
+                        {degreePlans.map((plan, i) => (<MenuItem value={i}>{plan.name}</MenuItem>))}
+                    </Select>
+                </FormControl>
+            );
+        }
+
         // Render semester select with error if semester is not found
         function semesterSelect(): (undefined | JSX.Element) {
             if (semester) return (
@@ -64,7 +74,6 @@ export function SemesterCalendar() {
                     <InputLabel id="semester-select">Semester</InputLabel>
                     <Select
                         id="outlined-input"
-                        className="example"
                         labelId="semester-select"
                         value={selectedSem}
                         onChange={e => {setSem(e.target.value as string);}}
@@ -79,7 +88,6 @@ export function SemesterCalendar() {
                     <Select
                         error
                         id="outlined-input"
-                        className="example"
                         labelId="semester-select"
                         value={selectedSem}
                         onChange={e => {setSem(e.target.value as string);}}
@@ -119,6 +127,9 @@ export function SemesterCalendar() {
         return (
             <div className="flex items-center">
                 <div className="ml-2 mr-6">
+                    {degreePlanSelect()}
+                </div>
+                <div className="mr-6">
                     {semesterSelect()}
                 </div>
                 <div className="mr-6">
@@ -130,10 +141,10 @@ export function SemesterCalendar() {
 
     function renderCalendar(): JSX.Element {
         const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-        const colors = ["rgb(227, 23, 10)", ];
+        const colors = ["rgb(227, 23, 10)", "rgb(169, 229, 187)", "rgb(252, 246, 177)", "rgb(247, 179, 43)", "rgb(56, 145, 166)", "rgb(154, 196, 248)", "rgb(171, 146, 191)", "rgb(175, 77, 152)", "rgb(89, 210, 254)", "rgb(56, 102, 65)", "rgb(134, 22, 87)"];
 
         // Generate column of time markers
-        const operatingHours = ["7:00am", "8:00am", "9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm"];
+        const operatingHours = ["7AM", "8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
         const timeBlocks = operatingHours.map((hour, i) => (
             <div className="blockStyle">
                 {hour}
@@ -149,12 +160,43 @@ export function SemesterCalendar() {
             </div>
         ));
 
+        // Populate course times for each day
+        //TODO: Check date boundaries for different weeks
+        function populateCourses(day: number): JSX.Element[] {
+            function parseTime(time: string): Date {
+                return new Date(0, 0, 0, 
+                    Number(time.split(":")[0])%12 + (time.split(" ")[1] === "PM" ? 12 : 0),
+                    Number(time.split(":")[1].split(" ")[0]));
+            }
+
+            var meetings: JSX.Element[] = [];
+            courses.forEach((course, i) => {
+                if (!course.section || course.section.meetings.length == 0) return;
+                
+                course.section?.meetings.forEach(meeting => {
+                    if (meeting.days.find(d => days[day].startsWith(d))) {
+                        const start = parseTime(meeting.startTime);
+                        const end = parseTime(meeting.endTime);
+                        const duration = (end.getTime() - start.getTime()) / 1000 / 60;
+                        const top = (start.getTime() - (new Date(0,0,0,7)).getTime()) /1000 / 60;
+                        meetings.push(
+                            <div className="CourseStyle" style={{top: top+"px", height: duration+"px", backgroundColor: colors[i%colors.length]}}>
+
+                            </div>
+                        );
+                    }
+                });
+            });
+            return meetings;
+        }
+
         // Generate columns for each day
         const columns = days.map((day, i) => (
             <div className="w-full">
                 <CardHeader title={day} className="text-center h-12 rad-4 bg-neutral-700 text-white" />
                 <div className="dayStyle">
                     {calendarGrid}
+                    {populateCourses(i)}
                 </div>
             </div>
         ));
