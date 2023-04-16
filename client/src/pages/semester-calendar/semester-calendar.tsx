4058bc0913aaf318fb1e44da8671ef9b985672d8
SnoringDragon <89707895+SnoringDragon@users.noreply.github.com>
2023-04-16 15:21:08 -0400

    Render calendar background

--- client/src/pages/semester-calendar/semester-calendar.tsx
+++ client/src/pages/semester-calendar/semester-calendar.tsx
@@ -3,6 +3,10 @@ import { Layout } from "../../components/layout/layout";
 import { useEffect, useRef, useState } from "react";
 import SemesterService from "../../services/SemesterService";
 import { Semester } from "../../types/semester";
+import { DegreePlan } from "../../types/degree-plan";
+import DegreePlanService from "../../services/DegreePlanService";
+import { UserCourse } from "../../types/user-course";
+import "./semester-calendar.css";
 
 export function SemesterCalendar() {
     // Semester Selection
@@ -16,28 +20,47 @@ export function SemesterCalendar() {
     const [selectedYear, setYear] = useState<number>(new Date().getFullYear());
     const [semesters, setSemesters] = useState<Semester[]>([]);
     const [semester, setSemester] = useState<Semester>();
+    const [degreePlans, setDegreePlans] = useState<DegreePlan[]>([]);
+    const degreePlan = useRef<DegreePlan>();
+    const [courses, setCourses] = useState<UserCourse[]>([]);
     
-    // Get semesters from database on page load
+    // Get semesters and degree plans from database on page load
     useEffect(() => {
+        DegreePlanService.getPlans().then(res => setDegreePlans(res.degreePlans));
         SemesterService.getSemesters().then(res => setSemesters(res));
     }, []);
+
+    // Get degree plan for selected degree plan
+    useEffect(() => {
+        if (degreePlans.length > 0) degreePlan.current = degreePlans[0];
+        else degreePlan.current = undefined;
+    }, [degreePlans]);
 
     // Update semester when semester or year is changed
     useEffect(() => {
         setSemester(semesters.find(s => s.semester === selectedSem && s.year === selectedYear));
     }, [selectedSem, selectedYear, semesters]);
 
-    // Get courses for selected semester
+    // Get courses for selected degreeplan and semester
     useEffect(() => {
-        //TODO: Get courses for semester and year
-    }, [semester]);
+        if (!degreePlan.current) return;
+        var courses: UserCourse[] = [];
+        degreePlan.current.courses.forEach(course => {
+            if (course.semester) {
+                if (course.semester === selectedSem && course.year === selectedYear) courses.push(course);
+            }
+        });
+        setCourses(courses);
+    }, [semester, degreePlan.current]);
     
     // Render semester selection with error if semester is not found
     function renderSemesterSelection(): (undefined | JSX.Element) {
-        if (semester) return (
-            <div className="flex items-center">
-                {/* Select semester */}
-                <FormControl className="ml-2 mr-6">
+        const semesterOptions = ["Spring", "Summer", "Fall", "Winter"].map(semester => (<MenuItem value={semester}>{semester}</MenuItem>));
+        
+        // Render semester select with error if semester is not found
+        function semesterSelect(): (undefined | JSX.Element) {
+            if (semester) return (
+                <FormControl>
                     <InputLabel id="semester-select">Semester</InputLabel>
                     <Select
                         id="outlined-input"
@@ -46,28 +69,12 @@ export function SemesterCalendar() {
                         value={selectedSem}
                         onChange={e => {setSem(e.target.value as string);}}
                     >
-                        <MenuItem value="Fall">Fall</MenuItem>
-                        <MenuItem value="Winter">Winter</MenuItem>
-                        <MenuItem value="Spring">Spring</MenuItem>
-                        <MenuItem value="Summer">Summer</MenuItem>
+                        {semesterOptions}
                     </Select>
                 </FormControl>
-                {/* Select year */}
-                <div className="mr-6">
-                    <TextField
-                        id="year"
-                        style={{ width: 60 }}
-                        type="number"
-                        label="Year"
-                        value={selectedYear}
-                        onChange={e => {setYear(Number(e.target.value));}}
-                    />
-                </div>
-            </div>);
-        else return (
-            <div className="flex items-center">
-                {/* Select semester */}
-                <FormControl className="ml-2 mr-6">
+            );
+            else return (
+                <FormControl>
                     <InputLabel error id="semester-select">Semester</InputLabel>
                     <Select
                         error
@@ -77,49 +84,94 @@ export function SemesterCalendar() {
                         value={selectedSem}
                         onChange={e => {setSem(e.target.value as string);}}
                     >
-                        <MenuItem value="Fall">Fall</MenuItem>
-                        <MenuItem value="Winter">Winter</MenuItem>
-                        <MenuItem value="Spring">Spring</MenuItem>
-                        <MenuItem value="Summer">Summer</MenuItem>
+                        {semesterOptions}
                     </Select>
                 </FormControl>
-                {/* Select year */}
-                <div className="mr-6">
-                    <TextField
-                        error
-                        id="year"
-                        style={{ width: 60 }}
-                        type="number"
-                        label="Year"
-                        value={selectedYear}
-                        onChange={e => {setYear(Number(e.target.value));}}
-                    />
-                </div>
-                <div>
-                    <span className="text-red-500">The selected semester does not have available scheduling information</span>
-                </div>
-            </div>);
-    }
+            );
+        }
 
-    function renderCalendar(): JSX.Element {
-        const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-        const headers = days.map(day => (
-            <Card className="w-full h-full">
-                <CardHeader title={day} className="text-center h-12 bg-zinc-800 text-white" />
-                <CardContent className="h-full">
-                    {/* TODO: Render courses for day */}
-                </CardContent>
-            </Card>
-        ));
+        // Render year text field with error if semester is not found
+        function yearTextField(): (undefined | JSX.Element) {
+            if (semester) return (
+                <TextField
+                    id="year"
+                    style={{ width: 60 }}
+                    type="number"
+                    label="Year"
+                    value={selectedYear}
+                    onChange={e => {setYear(Number(e.target.value));}}
+                />
+            );
+            else return (
+                <TextField
+                    error
+                    id="year"
+                    style={{ width: 60 }}
+                    type="number"
+                    label="Year"
+                    value={selectedYear}
+                    onChange={e => {setYear(Number(e.target.value));}}
+                />
+            );
+        }
 
+        // Render semester selection
         return (
-            <div className="flex mt-6">
-                {/* TODO: Time reference column */}
-                {headers}
+            <div className="flex items-center">
+                <div className="ml-2 mr-6">
+                    {semesterSelect()}
+                </div>
+                <div className="mr-6">
+                    {yearTextField()}
+                </div>
             </div>
         );
     }
 
+    function renderCalendar(): JSX.Element {
+        const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+        const colors = ["rgb(227, 23, 10)", ];
+
+        // Generate column of time markers
+        const operatingHours = ["7:00am", "8:00am", "9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm"];
+        const timeBlocks = operatingHours.map((hour, i) => (
+            <div className="blockStyle">
+                {hour}
+            </div>
+        ));
+
+        // Generate grid for each day
+        const calendarGrid = operatingHours.map((hour, i) => (
+            <div className="MajorGridStyle" style={{top: (60*i)+"px"}}>
+                <div className="MinorGridStyle" style={{top: "15px"}} />
+                <div className="MinorGridStyle" style={{top: "30px"}} />
+                <div className="MinorGridStyle" style={{top: "45px"}} />
+            </div>
+        ));
+
+        // Generate columns for each day
+        const columns = days.map((day, i) => (
+            <div className="w-full">
+                <CardHeader title={day} className="text-center h-12 rad-4 bg-neutral-700 text-white" />
+                <div className="dayStyle">
+                    {calendarGrid}
+                </div>
+            </div>
+        ));
+        
+        // Combine elements into one element
+        return (
+            <div className="flex mt-6">
+                <div className="inline-block h-full">
+                    <CardHeader className="h-12 w-0" />
+                    {timeBlocks}
+                </div>
+                {columns}
+            </div>
+        );
+    }
+
+    // Render page
     return (<Layout>
         {/* Main body */}
         <div className="overfill-auto flex flex-grow h-full">
@@ -129,9 +181,7 @@ export function SemesterCalendar() {
                 <Card className="mt-4">
                     <CardHeader title="Semester Calendar" className="text-center h-10 bg-zinc-800 text-white" />
                     <CardContent>
-                        {/* Semester selection */}
                         {renderSemesterSelection()}
-                        {/* Calendar */}
                         {renderCalendar()}
                     </CardContent>
                 </Card>
