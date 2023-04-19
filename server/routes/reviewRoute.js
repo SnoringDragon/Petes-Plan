9a7f90288d2c79ec09050223f556ab29a6db40c1
anushka48 <sharm495@purdue.edu>
2023-04-19 13:06:47 -0400

    Finished review-service with proper imports

--- server/routes/reviewRoute.js
+++ server/routes/reviewRoute.js
@@ -1,15 +1,20 @@
 const { Router } = require('express');
-const Review = require('../services/review-service.js');
-
 const mongoose = require('mongoose');
+
+const Course = require('../models/courseModel');
+const Instructor = require('../models/instructorModel');
+const {Rating} = require("../models/ratingModel");
+const toTitleCase = require("../utils/title-case");
+const ReviewService = require("../services/review-service");
 
 module.exports = app => {
     const router = Router();
-    router.get('/', async (req, res) => {
-        if (typeof req.query.weights !== 'object' || typeof req.query.assignments !== 'object') {
+    const { authenticate } = require('../middleware/authenticate');
+    router.get('/saveReview', async (req, res) => {
+        if (typeof req.query.in_email !== 'string' || typeof req.query.in_dateSubmitted !== 'string' || typeof req.query.in_courseSubject !== 'string' || typeof req.query.in_courseID !== 'string' || typeof req.query.instructor_fname !== 'string' || typeof req.query.instructor_lname !== 'string' || typeof req.query.in_wouldTakeAgain !== 'boolean' || typeof req.query.rating !== 'string' || typeof req.query.rating !== 'number' || typeof req.query.comment !== 'string' || typeof req.query.in_grade !== 'string') {
             return res.status(400).json({ message: 'invalid input' });
         }
-        const reviews = await Review.calculateGrade(instructor = req.query.instructor, course = req.query.course);
+        await ReviewService.saveReview(instructor = req.query.instructor, course = req.query.course);
 
         return reviews;
     });
--- server/services/review-service.js
+++ server/services/review-service.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
+const {Rating} = require('../models/ratingModel');
+const User = require('../models/userModel');
+const Instructor = require('../models/instructorModel');
+const Course = require('../models/courseModel');
 
-async function saveReviews(in_email, in_dateSubmitted, in_courseSubject, in_courseID, instructor_fname, instructor_lname, in_wouldTakeAgain, rating, comment, in_grade) {
+async function saveReview(in_email, in_dateSubmitted, in_courseSubject, in_courseID, instructor_fname, instructor_lname, in_wouldTakeAgain, rating, comment, in_grade) {
     let instructor_ret = await Instructor.findOne({ firstname: instructor_fname, lastname: instructor_lname });
     let user = await User.findOne({ email: in_email}); //don't need this yet, but maybe in the future?
     let course_ret = await Course.findOne({ subject: in_courseSubject, courseId: in_courseID});
-    let review = await degreeModel.create({ dateSubmitted : in_dateSubmitted, instructor: instructor_ret, course: course_ret, quality: rating, review: comment,  wouldTakeAgain: in_wouldTakeAgain, grade: in_grade});
+    let review = await Rating.create({ dateSubmitted : in_dateSubmitted, instructor: instructor_ret, course: course_ret, quality: rating, review: comment,  wouldTakeAgain: in_wouldTakeAgain, grade: in_grade});
 }
 
 //ret type: 2D array
