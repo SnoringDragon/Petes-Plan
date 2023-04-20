8d2b276b2fa362abe4509bfa2a35f44d2f602df4
anushka48 <sharm495@purdue.edu>
2023-04-20 17:05:27 -0400

    Parsing CLEP table data

--- server/scripts/fetch-clep.js
+++ server/scripts/fetch-clep.js
@@ -8,20 +8,29 @@ function getClepData() {
   .then(response => response.text())
   .then(content => {
     // parse the content using DOMParser
-    //cheerio - selector
-    var selector = cheerio.load(content)
-    // var parser = new DOMParser();
-    var htmlDoc = parser.parseFromString(content, 'text/html');
+    //cheerio
+    var $ = cheerio.load(content)
+    allClasses = []
+    allCourses = []
+    allHours = []
+    allScores =[]
+    
+    $("#inner-page-content > div > div > div.col-lg-8.main-content > div > table > tbody > tr > td:nth-child(1)").each((index, element) => {
+        allClasses.push([$(element).text()]);
+      });
+      console.log(allClasses[0])
+    }
     
     // find all elements with the tag name 'div' and class 'table-responsive'
-    var tableResponsiveElements = selector.getElementsByTagName('div');
-    for (var i = 0; i < tableResponsiveElements.length; i++) {
-      if (tableResponsiveElements[i].className == 'table-responsive') {
-        // do something with the element
-        console.log(tableResponsiveElements[i]);
-      }
-    }
-  });
+    // const tables = $('td');
+    // console.log((tables).length)
+    // for (var i = 0; i < tables.length; i++) {
+    //   if (tableResponsiveElements[i].className == 'table-responsive') {
+    //     // do something with the element
+    //     console.log(tableResponsiveElements[i]);
+    //   }
+    // }
+  );
     //gets table
     // fetch('https://www.admissions.purdue.edu/transfercredit/clep.php?_ga=2.156040763.1830545689.1680885941-405777200.1660331919').then((response) => response.text()).then((text) => console.log(text));
 //     $.get('https://stackoverflow.com/questions').then(function (html) {
