17ea6b3f754ee876e95200ec1a12e269116f1bd7
anushka48 <sharm495@purdue.edu>
2023-04-20 12:27:59 -0400

    Removed redundant code with RMP

--- server/services/review-service.js
+++ server/services/review-service.js
@@ -11,33 +11,33 @@ async function saveReview(in_email, in_dateSubmitted, in_courseSubject, in_cours
     let review = await Rating.create({ dateSubmitted : in_dateSubmitted, instructor: instructor_ret, course: course_ret, quality: rating, review: comment,  wouldTakeAgain: in_wouldTakeAgain, grade: in_grade});
 }
 
-//ret type: 2D array
-async function getReviewsByProfessor(instructor_fname, instructor_lname) {
-    let instructor_ret = await Instructor.findOne({ firstname: instructor_fname, lastname: instructor_lname });
-    let reviews = await ratingModel.find({ instructor: instructor_ret});
-    reviews = res.json(reviews);
-    //process reviews
-    let ret_reviews = "{ \"reviews\" : [";
-    for (let i = 0; i < reviews.length; i++) {
-        ret_reviews += JSON.stringify(reviews[i]);
-        ret_reviews += ","
-    }
-    ret_reviews+="]}"
-    return ret_reviews;
-}
+// //ret type: 2D array
+// async function getReviewsByProfessor(instructor_fname, instructor_lname) {
+//     let instructor_ret = await Instructor.findOne({ firstname: instructor_fname, lastname: instructor_lname });
+//     let reviews = await ratingModel.find({ instructor: instructor_ret});
+//     reviews = res.json(reviews);
+//     //process reviews
+//     let ret_reviews = "{ \"reviews\" : [";
+//     for (let i = 0; i < reviews.length; i++) {
+//         ret_reviews += JSON.stringify(reviews[i]);
+//         ret_reviews += ","
+//     }
+//     ret_reviews+="]}"
+//     return ret_reviews;
+// }
 
-async function getReviewsByCourse(in_courseSubject, in_courseID) {
-    let course_ret = await course.findOne({ subject: in_courseSubject, courseId: in_courseID });
-    let reviews = await ratingModel.find({ course: course_ret});
-    reviews = res.json(reviews);
-    //process reviews
-    let ret_reviews = "{ \"reviews\" : [";
-    for (let i = 0; i < reviews.length; i++) {
-        ret_reviews += JSON.stringify(reviews[i]);
-        ret_reviews += ","
-    }
-    ret_reviews+="]}"
-    return ret_reviews;
-}
+// async function getReviewsByCourse(in_courseSubject, in_courseID) {
+//     let course_ret = await course.findOne({ subject: in_courseSubject, courseId: in_courseID });
+//     let reviews = await ratingModel.find({ course: course_ret});
+//     reviews = res.json(reviews);
+//     //process reviews
+//     let ret_reviews = "{ \"reviews\" : [";
+//     for (let i = 0; i < reviews.length; i++) {
+//         ret_reviews += JSON.stringify(reviews[i]);
+//         ret_reviews += ","
+//     }
+//     ret_reviews+="]}"
+//     return ret_reviews;
+// }
 
-module.exports = {saveReview, getReviewsByProfessor, getReviewsByCourse};
+module.exports = {saveReview};
