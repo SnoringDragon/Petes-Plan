418a00e60ae48a65d6151432782cb60cec3f939e
SnoringDragon <89707895+SnoringDragon@users.noreply.github.com>
2023-04-20 00:53:28 -0400

    Added week selection internally

--- client/src/pages/semester-calendar/semester-calendar.tsx
+++ client/src/pages/semester-calendar/semester-calendar.tsx
@@ -31,13 +31,20 @@ export function SemesterCalendar() {
     const [courses, setCourses] = useState<UserCourse[]>([]);
     const [colWidth, setColWidth] = useState<CSSProperties>({ width: "14%" });
     const [coursesByDay, setCoursesByDay] = useState<JSX.Element[][]>([[], [], [], [], [], [], []]);
-    const [weekBounds, setWeekBounds] = useState<Date[]>([new Date(), new Date()]);
+    const [selectedWeek, setSelectedWeek] = useState<Date>(); //TODO: Set to lowest date in courses
+    const [dateBounds, setDateBounds] = useState<(Date | undefined)[]>([undefined, undefined]); //TODO: Set to lowest and highest date in courses
+    //TODO: Add week selection
 
     // Get a Date object from a time string
     function parseTime(time: string): Date {
         return new Date(0, 0, 0, 
             Number(time.split(":")[0])%12 + (time.split(" ")[1] === "PM" ? 12 : 0),
             Number(time.split(":")[1].split(" ")[0]));
+    }
+
+    function parseDate(date: string): Date {
+        const {0: month, 1: day, 2: year} = date.split("/");
+        return new Date(parseInt(year), parseInt(month)-1, parseInt(day));
     }
     
     // Get semesters and degree plans from database on page load and set event listeners
@@ -66,14 +73,36 @@ export function SemesterCalendar() {
         var courses: UserCourse[] = [];
         degreePlans[degreePlan].courses.forEach(course => {
             if (course.semester) {
-                if (course.semester === selectedSem && course.year === selectedYear) courses.push(course);
+                if (course.semester === selectedSem && course.year === selectedYear) {
+                    courses.push(course);
+
+                    // Update date bounds
+                    if (course.section) {
+                        //TODO: Add support for courses with multiple sections
+                        course.section.meetings.forEach((meeting: Meeting) => {
+                            var startDate: Date = parseDate(meeting.startDate);
+                            var endDate: Date = parseDate(meeting.endDate);
+                            if (!dateBounds[0] || startDate < dateBounds[0]) setDateBounds([startDate, dateBounds[1]]);
+                            if (!dateBounds[1] || endDate > dateBounds[1]) setDateBounds([dateBounds[0], endDate]);
+                        });
+                    } else {
+                        //TODO: Add support for courses with no section
+                    }
+                }
             }
         });
+
+        // Set selected week to first day of semester
+        if (dateBounds[0]) {
+            var firstDay: Date = new Date(dateBounds[0].getTime());
+            firstDay.setDate(firstDay.getDate() - dateBounds[0].getDay());
+            setSelectedWeek(firstDay);
+        }
+
         setCourses(courses);
     }, [semester, degreePlan, degreePlans]);
 
     // Load courses to internal array by day
-    //TODO: Check date boundaries for different weeks
     useEffect(() => {
         // Get the name of the instructor for a section meeting time
         function getInstructorName(meeting: Meeting): string {
@@ -103,6 +132,12 @@ export function SemesterCalendar() {
                         // Add meeting to day
                         var dayIndex: number = days.findIndex(d => d.toLocaleLowerCase().includes(day.toLocaleLowerCase()));
                         if (dayIndex < 0) return; // Skip days that are not in the calendar
+
+                        // Check if meeting in current week
+                        if (!selectedWeek) return; // Exit if no week selected
+                        var date: Date = new Date(selectedWeek.getTime());
+                        date.setDate(date.getDate() + dayIndex); // Set date to day of week
+                        if (date < parseDate(meeting.startDate) || date > parseDate(meeting.endDate)) return;
 
                         // Add meeting to day
                         coursesByDay[dayIndex].push(<div key={parseInt(`${i}${j}${dayIndex}`)}>
