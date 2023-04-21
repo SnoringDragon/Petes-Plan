50bb4e77ebba0b74a679de973a3fa846bd80d65a
sk-1982 <13491412+sk-1982@users.noreply.github.com>
2023-04-21 00:24:44 -0400

    count ap courses in course history

--- server/models/userCourseModel.js
+++ server/models/userCourseModel.js
@@ -9,7 +9,8 @@ const userCourseSchema = new mongoose.Schema({
     instructor: { type: mongoose.Schema.Types.ObjectId, ref: 'Instructor' },
     section: { type: mongoose.Schema.Types.ObjectId, ref: 'Section' },
     meetingTime: Number,
-    courseData: { type: mongoose.Schema.Types.ObjectId, ref: 'Course' }
+    courseData: { type: mongoose.Schema.Types.ObjectId, ref: 'Course' },
+    source: String
 })
 
 exports.schema = userCourseSchema;
