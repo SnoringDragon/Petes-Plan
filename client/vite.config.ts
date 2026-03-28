098bf4e8c0d6255ac475ca4cd71c03b7def26f2f
Carey Lin <lin1433@purdue.edu>
2026-03-29 04:39:18 +0800

    fix allowedHosts for AWS deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,5 +11,11 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 4173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    // @ts-ignore
+    allowedHosts: 'all'
   }
 })
